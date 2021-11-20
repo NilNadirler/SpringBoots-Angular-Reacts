e626aa5ce6a2c404b6872a2e2a5d938c48658e24
Nil <nilnadirler@gmail.com>
2021-11-21 01:11:22 +0300

    SpringBoots-Thymleaf-React-Angular

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/98/703cbed5c047001c190fbc1e58be3538
@@ -0,0 +1,15 @@
+package net.javaguides.springboot.web.dto;
+
+import lombok.Data;
+
+@Data
+public class UserRegistrationDto {
+
+	private String firstName;
+	
+	private String lastName;
+	
+	private String password;
+	
+	private String email;
+}
---
+++ springboot-thymeleaf-registration-login/src/test/java/net/javaguides/springbootregistrationlogin1/RegistrationLoginSpringBootSecurityThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.springbootregistrationlogin1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationLoginSpringBootSecurityThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
